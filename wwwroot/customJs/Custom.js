ce9bb79412a30a87d089a886c0c01cd223590789
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2023-12-27 01:40:12 +0630

    Select 2 Set up

---
+++ wwwroot/customJs/Custom.js
@@ -0,0 +1,9 @@
+﻿window.initializeSelect2 = (elementId) => {
+    console.log("ID : ", elementId);
+    $('#' + elementId).select2({ multiple:true});
+    $('#' + elementId).on('select2:opening select2:closing', function (event) {
+        var $searchfield = $(this).parent().find('.select2-search__field');
+        $searchfield.prop('disabled', true);
+    });
+    console.log("Selected.");
+};
